6e9faaad2977a125df560f01478ef5d36634f9ba
yamamoto-yj <y-yamamoto@integralworks.co.jp>
2025-06-12 07:53:06 +0900

    プロジェクト ファイルを追加します。

---
+++ OnlineSales/Login.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="RakuichiRakuza.OnlineSales.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>楽市楽座</title>
+    <link href="../Css/login.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="login-container">
+    <h1>ログイン</h1>
+    <form id="form1" runat="server">
+        <div class="form-group">
+            <asp:Label ID="lblUser" runat="server" Text="アカウント"></asp:Label>
+            <asp:TextBox ID="txtAcnt" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <asp:Label ID="lblPswd" runat="server" Text="パスワード"></asp:Label>
+            <asp:TextBox ID="txtPswd" runat="server" type="password"></asp:TextBox>
+        </div>
+        <asp:Button ID="btnLogin" runat="server" Text="ログイン" class="login-button" />
+    </form>
+    </div>
+</body>
+</html>
---
+++ OnlineSales/Login.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、正しくない動作の原因になる可能性があり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace RakuichiRakuza.OnlineSales
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblUser コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUser;
+
+        /// <summary>
+        /// txtAcnt コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAcnt;
+
+        /// <summary>
+        /// lblPswd コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPswd;
+
+        /// <summary>
+        /// txtPswd コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPswd;
+
+        /// <summary>
+        /// btnLogin コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+    }
+}
